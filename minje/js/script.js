b0d08fc4072f8abbb4884cd648ce24b677ab4639
minje <alswp0910@naver.com>
2023-05-04 16:15:46 +0900

    branch test

--- minje/js/script.js
+++ minje/js/script.js
@@ -1,6 +1,15 @@
-function changeMainFrameSrc() {
+function changeMainFrameCal() {
   var mainFrame = document.getElementById("mainFrame");
   var newUrl = "table.html";
+  mainFrame.src = newUrl;
+  mainFrame.onload = function() {
+    console.log("iframe 로드 완료");
+  };
+}
+
+function changeMainFrameMain(){
+  var mainFrame = document.getElementById("mainFrame");
+  var newUrl = "";
   mainFrame.src = newUrl;
   mainFrame.onload = function() {
     console.log("iframe 로드 완료");
